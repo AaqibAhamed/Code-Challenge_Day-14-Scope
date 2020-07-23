d32863ce48ff085617fed31409f51f7b302c560b
AaqibGitBit <ahamedaaqib96@gmail.com>
2020-07-23 20:58:19 +0530

    Scope

---
+++ Difference.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Code_Challenge_Day_14_Scope
+{
+    public class Difference
+    {
+        private int[] elements;
+        public int maximumDifference;
+
+        public Difference(int [] a)
+        {
+            elements = a;
+        }
+
+        public void computeDifference()
+        {
+            Array.Sort(this.elements);
+
+            maximumDifference = elements.Max() - elements.Min();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Code_Challenge_Day_14_Scope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Difference d = new Difference(a);
+
+            d.computeDifference();
+
+            Console.WriteLine(d.maximumDifference);
+            
+
+            
+        }
+    }
+}
